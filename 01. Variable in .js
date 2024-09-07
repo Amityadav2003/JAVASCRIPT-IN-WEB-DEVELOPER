0cbcea86c2f2521fe50c38621f3353dfefa305c7
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-07 21:28:25 +0530

    Create 01. Variable in .js

---
+++ 01. Variable in .js
@@ -0,0 +1,16 @@
+//var
+var h=10;
+var i=5;
+var j=h+i;
+console.log(j);
+
+//let
+let x=10;
+let y=25;
+let z=x+y;
+console.log(z);
+//const
+const a=10;
+const b=15;
+const c=a+b;
+console.log(c);
