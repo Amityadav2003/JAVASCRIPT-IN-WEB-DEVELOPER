2a7867399b2b309b7d8ef37ff1367b9ad244303e
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 21:03:16 +0530

    Create 10. if-else condition.js

---
+++ 10. if-else condition.js
@@ -0,0 +1,14 @@
+let marks=90;
+if(marks>=90){
+    console.log("Grade A+");
+}
+else if(marks>=70){
+    console.log("Grade B");
+}
+else if(marks>=33){
+    console.log("Grade C");
+}
+else{
+    console.log("Fail !..");
+}
+
