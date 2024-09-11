b342afd981c5fd8e15dd74e3300a41ed2998abb7
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 21:21:16 +0530

    Create 12. Switch Statement.js

---
+++ 12. Switch Statement.js
@@ -0,0 +1,14 @@
+let color="Red";
+switch(color){
+    case "Green":
+        console.log("GO !");
+        break;
+    case "Red":
+        console.log("Stop !");
+        break;
+    case "Yellow":
+        console.log("GO Slow !");
+        break;
+    default:
+        console.log("NO light system !")
+}
