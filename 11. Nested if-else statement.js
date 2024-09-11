852285102b1c89ed67ce342485c897b8112e22b6
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 21:11:25 +0530

    Create 11. Nested if-else statement.js

---
+++ 11. Nested if-else statement.js
@@ -0,0 +1,17 @@
+let marks=60;
+if(marks>=33){
+ if(marks>=70){
+     console.log("Grade B");
+    }
+else if(marks>=60){
+    console.log("Grade C");
+    }
+else{
+    console.log("pass !..")
+}
+}
+
+else{
+    console.log("Fail !..");
+}
+
