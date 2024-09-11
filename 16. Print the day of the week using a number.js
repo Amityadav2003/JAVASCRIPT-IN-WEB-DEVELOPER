c0ca5b82b881eca77db12ede56c2326d8e39a64f
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 21:45:54 +0530

    Create 16. Print the day of the week using a number.js

---
+++ 16. Print the day of the week using a number.js
@@ -0,0 +1,23 @@
+let day=1;
+switch(day){
+    case 1:
+        console.log("monday !");
+        break;
+    case 2:
+        console.log("tuesday !");
+        break;
+    case 3:
+        console.log("wenesday !");
+        break;
+    case 4:
+        console.log("tresday !");
+        break;
+    case 5:
+        console.log("friday !");
+        break;
+    case 6:
+        console.log("saturaday !");
+        break;
+    default:
+        console.log("sanday  !")
+}
