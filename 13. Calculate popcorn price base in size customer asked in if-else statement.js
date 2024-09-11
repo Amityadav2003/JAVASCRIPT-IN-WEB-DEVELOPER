079d25129524c6d323c1f6e78339e5f9c4183eb3
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 21:30:32 +0530

    Create 13. Calculate popcorn price base in size customer asked in if-else statement.js

---
+++ 13. Calculate popcorn price base in size customer asked in if-else statement.js
@@ -0,0 +1,13 @@
+let size="XL";
+if(size==="XL"){
+    console.log("Price Rs 250");
+}
+else if(size==="L"){
+    console.log("Price Rs 200");
+}
+else if(size==="M"){
+    console.log("Price Rs 150");
+}
+else{
+    console.log("Price Rs 50");
+}
