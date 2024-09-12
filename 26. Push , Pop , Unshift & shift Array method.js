0a056efb1b1a7f0cbe2aae72a218c7faa1d4590a
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-12 21:18:34 +0530

    Create 26. Push , Pop , Unshift & shift Array method.js

---
+++ 26. Push , Pop , Unshift & shift Array method.js
@@ -0,0 +1,16 @@
+let color=["red","blue","black","yellow"];
+//push method
+color.push("orange");
+console.log(color);
+
+//pop method
+color.pop();
+console.log(color);
+
+//unshift method
+color.unshift("orange")
+console.log(color);
+
+//shift method
+color.shift();
+console.log(color);
