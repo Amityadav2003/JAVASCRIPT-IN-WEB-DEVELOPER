afb4c27d30f7b2fd0785fd3aa9c5d88968b9616e
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-12 21:38:22 +0530

    Create 27. Indexof , Includes , Concat , Reverse  Array method.js

---
+++ 27. Indexof , Includes , Concat , Reverse  Array method.js
@@ -0,0 +1,17 @@
+let color=["red","blue","black","yellow"];
+//indexof method
+let index=color.indexof["blue"];
+console.log(index);
+
+
+//include method
+console.log(color.includes("red"));
+
+//concat method
+let primary=["red","blue","black","yellow"];
+let secondary=["orange","green","violet"];
+
+console.log(primary.concat(secondary));
+
+//reverse method
+console.log(primary.reverse());
