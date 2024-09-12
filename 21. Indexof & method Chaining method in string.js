a2547c9dc1bc9cbcac06d736704914664dbf9a72
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-12 20:38:13 +0530

    Create 21. Indexof & method Chaining method in string.js

---
+++ 21. Indexof & method Chaining method in string.js
@@ -0,0 +1,16 @@
+let msg="AmitYadav";
+//indexof method
+let newstr=msg.indexOf("Amit");
+let newstr1=msg.indexOf("mit");
+let newstr2=msg.indexOf("A");
+let newstr3=msg.indexOf("k");
+
+console.log(newstr);
+console.log(newstr1);
+console.log(newstr2);
+console.log(newstr3);
+
+
+// method chaining
+let newstr4=msg.toLowerCase().trim();
+console.log(newstr4)
