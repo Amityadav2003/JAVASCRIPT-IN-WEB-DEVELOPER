6dbb5a8556e265214adab08ab876bc8e2c96ed95
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-12 21:00:21 +0530

    Create 22. Slice & Replace String method.js

---
+++ 22. Slice & Replace String method.js
@@ -0,0 +1,14 @@
+let str="IloveCoding";
+// slice method
+let newstr= str.slice(5);
+let newstr1= str.slice(1,4);
+let newstr2= str.slice(-1);
+console.log(newstr);
+console.log(newstr1);
+console.log(newstr2);
+
+//replace method
+let newstr3=str.replace("love","do");
+let newstr4=str.replace("o","x");
+console.log(newstr3);
+console.log(newstr4);
