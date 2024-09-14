8de5dd6a86313238deb7576739db6155b3cb3692
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-14 20:57:55 +0530

    Create 38. Nested loop with Nested Array.js

---
+++ 38. Nested loop with Nested Array.js
@@ -0,0 +1,7 @@
+let fruits=[["mango","apple"],["banana","litchi","orange"]];
+for(let i=0;i<fruits.length;i++){
+    console.log(`list # ${i}`);
+    for(let j=0;j<fruits[0].length;j++){
+        console.log(fruits[i][j]);
+    }
+}
