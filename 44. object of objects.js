861bcdda6f5704c3c0c48b534747d62259f29dff
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-14 21:44:07 +0530

    Create 44. object of objects.js

---
+++ 44. object of objects.js
@@ -0,0 +1,13 @@
+const student={
+    amit:{
+        age:21,
+        marks:94.4,
+        city:"Delhi"
+    },
+    Aman:{
+        age:25,
+        marks:90.4,
+        city:"Puna"
+    }
+};
+console.log(student);
