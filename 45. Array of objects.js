8dcbd6cf289a6df96903daa62a37dd85217f877d
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-14 21:46:32 +0530

    Create 45. Array of objects.js

---
+++ 45. Array of objects.js
@@ -0,0 +1,15 @@
+const student=[
+    {
+        name:"Amit",
+        age:21,
+        marks:94.4,
+        city:"Delhi"
+    },
+    {
+        name:"Aman",
+        age:25,
+        marks:90.4,
+        city:"Puna"
+    }
+];
+console.log(student);
