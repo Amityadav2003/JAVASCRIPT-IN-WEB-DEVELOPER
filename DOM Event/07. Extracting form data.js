80156ea7fa1a12ad0e0021075a485e6d174060a8
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-25 21:03:53 +0530

    Create 07. Extracting form data.js

---
+++ DOM Event/07. Extracting form data.js
@@ -0,0 +1,8 @@
+let form=document.querySelector("form");
+form.addEventListener("submit",function(event){
+event.preventDefault();
+
+let inp=document.querySelector("input");
+console.dir(inp);
+console.log(inp.value);
+});
