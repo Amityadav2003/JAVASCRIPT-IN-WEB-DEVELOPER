b64e6960c9ab3936fe305f66fca1d5753c31eb8b
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-25 11:41:43 +0530

    Create 08. Manipulating  using classlist.js

---
+++ DOM/08. Manipulating  using classlist.js
@@ -0,0 +1,13 @@
+
+let heading=document.querySelector('h2');
+// add classlist
+heading.classList.add("green");
+
+//remove classlist
+heading.classList.remove("green");
+
+// contains classlist
+console.dir(heading.classList.contains("green"));
+
+//toggle classlist
+heading.classList.toggle("green");
