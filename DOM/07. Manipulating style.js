ae01ec8852f4d7dd734292c6f85bc8c9760f32dc
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-25 11:27:26 +0530

    Create 07. Manipulating style.js

---
+++ DOM/07. Manipulating style.js
@@ -0,0 +1,13 @@
+// 1
+let heading=document.querySelector('h2');
+console.dir(heading);
+
+heading.style;
+heading.style.color='red';
+
+// ex=2
+
+let links=document.querySelectorAll(".box a");
+for(let i=0;i<links.length;i++){
+    links[i].style.color="red";
+}
