7d8292a2fe5e658b13655d48ac82aa900d298952
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-25 11:05:17 +0530

    Create 05. Innertext , TextContent & innerHTML properties in queryselector.js

---
+++ DOM/05. Innertext , TextContent & innerHTML properties in queryselector.js
@@ -0,0 +1,15 @@
+// innertext
+let para=document.querySelector('p');
+console.dir(para);
+console.log(para.innerText);
+
+//text content
+console.log(para.textContent);
+
+//inner html
+console.log(para.innerHTML);
+
+
+
+
+
