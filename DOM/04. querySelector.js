e5a123d7dc36ab5ba35767b289f5991a444d85db
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-25 10:58:52 +0530

    Create 04. querySelector.js

---
+++ DOM/04. querySelector.js
@@ -0,0 +1,15 @@
+// select first p element
+let para=document.querySelector('p');
+console.dir(para);
+
+// select first element with class=img1
+let img=document.querySelector('.img1');
+console.dir(img);
+
+// select first element with id=mainimg
+let mainimg=document.querySelector('#mainimg');
+console.dir(mainimg);
+
+// select all p element
+let para1=document.querySelectorAll('p');
+console.dir(para1);
